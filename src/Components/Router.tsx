dcd0687ead2ad31437bb1dc1ee61e9d8dd3a0b81
BeomjunKim <wka8963@gmail.com>
2020-07-14 22:42:55 +0900

    feat: 홈 컴포넌트를 라우터에 연결
    홈 컴포넌트를 라우터에 연결
    
    See also: #4

--- src/Components/Router.tsx
+++ src/Components/Router.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import Home from "../Routes/Home";
 
 const Router: React.FunctionComponent = () => (
   <BrowserRouter>
     <Switch>
+      <Route path="/" exact component={Home} />
       <Redirect from="*" to="/" />
     </Switch>
   </BrowserRouter>
