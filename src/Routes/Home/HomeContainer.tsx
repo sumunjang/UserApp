b6bb857ba51c0453336865d8222c75af0519e069
BeomjunKim <wka8963@gmail.com>
2020-07-14 22:30:28 +0900

    feat: 홈에서 로직을 처리하는 HomeContainer생성
    홈에서 로직을 처리하는 HomeContainer생성
    
    See also: #4

---
+++ src/Routes/Home/HomeContainer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import HomePresenter from "./HomePresenter";
+
+const HomeContainer: React.FunctionComponent = () => {
+  return <HomePresenter />;
+};
+
+export default HomeContainer;
