295531eb938bae7f3f93e76a4d2f82ab23efb0b2
BeomjunKim <wka8963@gmail.com>
2020-07-14 22:30:53 +0900

    feat: 홈에서 UI를 생성하는 HomePresenter생성
    홈에서 UI를 생성하는 HomePresenter생성
    
    See also: #4

---
+++ src/Routes/Home/HomePresenter.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const HomePresenter: React.FunctionComponent = () => {
+  return <div>hello!</div>;
+};
+
+export default HomePresenter;
