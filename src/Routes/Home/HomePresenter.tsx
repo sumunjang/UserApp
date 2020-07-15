c82754f21fef04b66441c117058c53117ab7edde
BeomjunKim <wka8963@gmail.com>
2020-07-15 14:01:55 +0900

    feat: 홈 UI 배치
    홈 UI배치만 하였음
    
    Resolves: #4

--- src/Routes/Home/HomePresenter.tsx
+++ src/Routes/Home/HomePresenter.tsx
@@ -3,8 +3,44 @@ import styled from "styled-components";
 
 const Container = styled.div``;
 
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Home = styled.div``;
+
+const Logo = styled.div`
+  margin-left: 50px;
+  margin-right: 50px;
+`;
+
+const Pass = styled.div``;
+
+const Search = styled.div``;
+
+const PlaceContainer = styled.div``;
+
+const PlaceCard = styled.div``;
+
+const GotoQRcode = styled.div``;
+
 const HomePresenter: React.FunctionComponent = () => {
-  return <Container>hello!</Container>;
+  return (
+    <Container>
+      <Header>
+        <Home>홈</Home>
+        <Logo>로고</Logo>
+        <Pass>출입증</Pass>
+      </Header>
+      <Search>장소 검색</Search>
+      <PlaceContainer>
+        <PlaceCard>시설이름 | 시설위치 | 방문날짜, 시간</PlaceCard>
+      </PlaceContainer>
+      <GotoQRcode>QR코드로 문진표 작성하기</GotoQRcode>
+    </Container>
+  );
 };
 
 export default HomePresenter;
