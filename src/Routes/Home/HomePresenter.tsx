e9e3706171ac944d87cbace020aacb8185f91649
BeomjunKim <wka8963@gmail.com>
2020-07-15 13:35:03 +0900

    fix: styled-component 미사용으로 인한 오류 수정
    styled-component로 container 하나를 만들어 사용해서 오류 수정
    
    See also: #4

--- src/Routes/Home/HomePresenter.tsx
+++ src/Routes/Home/HomePresenter.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 
+const Container = styled.div``;
+
 const HomePresenter: React.FunctionComponent = () => {
-  return <div>hello!</div>;
+  return <Container>hello!</Container>;
 };
 
 export default HomePresenter;
