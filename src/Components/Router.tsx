ec4896fa52df85b1930cf9761e5940d8a4b9efa6
BeomjunKim <wka8963@gmail.com>
2020-07-15 18:03:54 +0900

    feat: 라우터에 출입증 페이지 연결
    출입증 페이지에 접속할 수 있게 출입증페이지 연결
    
    See also: #8

--- src/Components/Router.tsx
+++ src/Components/Router.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Home from "../Routes/Home";
+import Pass from "../Routes/Pass";
 
 const Router: React.FunctionComponent = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Home} />
+      <Route path="/pass/:userid" component={Pass} />
       <Redirect from="*" to="/" />
     </Switch>
   </BrowserRouter>
