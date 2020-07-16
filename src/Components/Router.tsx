eb4a2beefa9675abb34bc40478d85c6d578a7ffb
BeomjunKim <wka8963@gmail.com>
2020-07-16 13:01:39 +0900

    feat: 방문장소 리스트 라우팅
    방문장소 리스트 라우터에 연결
    
    See also: #12

--- src/Components/Router.tsx
+++ src/Components/Router.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Home from "../Routes/Home";
-import Pass from "../Routes/Pass";
 import Search from "../Routes/Search";
 import Form from "../Routes/Form";
+import User from "../Routes/User";
 
 const Router: React.FunctionComponent = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Home} />
-      <Route path="/pass/:userid" component={Pass} />
       <Route path="/search" component={Search} />
       <Route path="/form" component={Form} />
+      <Route path="/user" component={User} />
       <Redirect from="*" to="/" />
     </Switch>
   </BrowserRouter>
--- src/Components/Routes.tsx
+++ src/Components/Routes.tsx
@@ -1,15 +1,17 @@
 const Home = "/";
 
-const Pass = "/pass/:id";
-
 const SearchMain = "/search";
 const SearchResult = "/search/result";
 
 const Form = "/form/:placeid";
 
+const UserPass = "/user/pass/:userid";
+const UserVisitedList = "/user/visited-list/:userid";
+
 export default {
   Home,
-  Pass,
+  UserPass,
+  UserVisitedList,
   SearchMain,
   SearchResult,
   Form,
