6461c1b984302960a452c5dd30e753ad5efa5d70
BeomjunKim <wka8963@gmail.com>
2020-07-16 11:38:39 +0900

    feat: 장소 검색페이지를 라우터에 연결
    장소 검색 페이지를 접속할 수 있게 연결
    
    See also: #9

--- src/Components/Router.tsx
+++ src/Components/Router.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Home from "../Routes/Home";
 import Pass from "../Routes/Pass";
+import Search from "../Routes/Search";
 
 const Router: React.FunctionComponent = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/pass/:userid" component={Pass} />
+      <Route path="/search" component={Search} />
       <Redirect from="*" to="/" />
     </Switch>
   </BrowserRouter>
